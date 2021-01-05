05758fb716e1bfe9f4413a2eae413e79f83e151d
RipaltaOriol <48039207+RipaltaOriol@users.noreply.github.com>
2021-01-05 19:24:29 +0100

    Public version

--- routes/calculator.js
+++
@@ -1,8 +0,0 @@
-let express     = require('express');
-let router      = express.Router({mergeParams: true});
-let calculator  = require('../controller/calculator');
-let middleware  = require('../middleware');
-
-router.post('/convert', middleware.isLoggedIn, calculator.convert)
-
-module.exports = router;
