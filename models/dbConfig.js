d7c43af70b50e0ecb4d3e4aad2704337f97375f9
RipaltaOriol <48039207+RipaltaOriol@users.noreply.github.com>
2021-01-25 11:14:58 -0500

    Deploying v1.0.11

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -2,22 +2,22 @@
 let mysql = require('mysql');
 
 // Connect to DB
-// var connection = mysql.createConnection({
-//   host    : 'db-production-neilit-1015-do-user-8160676-0.b.db.ondigitalocean.com',
-//   user    : 'doadmin',
-//   password: process.env.DB_PASS,
-//   port    : 25060,
-//   database: 'defaultdb',
-//   multipleStatements: true
-// });
-
 var connection = mysql.createConnection({
-  host    : process.env.DB_HOST,
-  user    : process.env.DB_USER,
+  host    : 'db-production-neilit-1015-do-user-8160676-0.b.db.ondigitalocean.com',
+  user    : 'doadmin',
   password: process.env.DB_PASS,
-  database: 'neilit_db',
+  port    : 25060,
+  database: 'defaultdb',
   multipleStatements: true
 });
+
+// var connection = mysql.createConnection({
+//   host    : process.env.DB_HOST,
+//   user    : process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: 'neilit_db',
+//   multipleStatements: true
+// });
 
 connection.connect((err) => {
   if (err) {
